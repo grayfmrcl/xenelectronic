e964269c79b41416718d187791677c74be1b62bb
Grayfield Moningkey <grayfieldmm@gmail.com>
2021-07-06 05:13:57 +0800

    set cascade when truncate category

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const { controllerWrapper } = require('../helpers/express');
 const controller = require('../controllers/category');
 
-/* GET home page. */
 router.get('/', controllerWrapper(controller.getList));
 
 module.exports = router;
