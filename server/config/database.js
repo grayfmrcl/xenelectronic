50f7a4a0203d67dfab52354a18fb0ffa725054ea
Grayfield Moningkey <grayfieldmm@gmail.com>
2021-07-07 07:51:17 +0800

    fix database config

--- server/config/database.js
+++ server/config/database.js
@@ -8,7 +8,11 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
   },
 };
